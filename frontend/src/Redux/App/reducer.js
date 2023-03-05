ae476af89507e98e4b52454b401af160bbf0c519
jitendra4545 <jitendra45.js45@gmail.com>
2023-03-05 19:02:39 +0530

    added

---
+++ frontend/src/Redux/App/reducer.js
@@ -0,0 +1,20 @@
+
+let initialState={
+    isAuth:false,
+    isLoading:false,
+    isError:false
+}
+
+
+const reducer=(state=initialState,action)=>{
+    const {type,payload}=action
+    switch(type){
+      
+        default:
+            return state
+    }
+ 
+}
+
+
+export {reducer}
---
+++ frontend/src/Redux/Auth/reducer.js
@@ -0,0 +1,21 @@
+
+let initialState={
+    isAuth:false,
+    isLoading:false,
+    isError:false,
+    token:""
+}
+
+
+const reducer=(state=initialState,action)=>{
+const {type,payload}=action
+    switch(type){
+      
+        default:
+            return state
+    }
+ 
+}
+
+
+export {reducer}
